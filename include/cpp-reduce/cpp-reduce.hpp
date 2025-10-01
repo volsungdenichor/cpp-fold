db7bdb939900ab4bdb7c4689c03d981f2c745d1e
krzysiek <krzysztof.muhleisen@gmail.com>
2025-10-01 21:34:47 +0200

    transducer chaining

--- include/cpp-reduce/cpp-reduce.hpp
+++ include/cpp-reduce/cpp-reduce.hpp
@@ -59,11 +59,11 @@ constexpr auto continue_with(T&& item) -> step_t<std::decay_t<T>>
     return { std::forward<T>(item), step_result_type::should_continue };
 }
 
-template <class State, class Func>
-struct folder_t
+template <class State, class Reducer>
+struct reducer_proxy_t
 {
     State state;
-    Func func;
+    Reducer reducer;
 
     template <class Range>
     auto operator()(Range&& range) const -> State
@@ -71,7 +71,7 @@ struct folder_t
         auto current = step_t<State>{ state };
         for (auto it = std::begin(range); it != std::end(range); ++it)
         {
-            current = std::invoke(func, std::move(current).get(), *it);
+            current = std::invoke(reducer, std::move(current).get(), *it);
             if (current.type() == step_result_type::should_break)
             {
                 break;
@@ -84,12 +84,12 @@ struct folder_t
 namespace detail
 {
 
-struct fold_fn
+struct reduce_fn
 {
-    template <class State, class Func>
-    constexpr auto operator()(State state, Func func) const -> folder_t<State, Func>
+    template <class State, class Reducer>
+    constexpr auto operator()(State state, Reducer reducer) const -> reducer_proxy_t<State, Reducer>
     {
-        return { std::move(state), std::move(func) };
+        return { std::move(state), std::move(reducer) };
     }
 };
 
@@ -111,7 +111,7 @@ struct logical_sum_fn
     };
 
     template <class Pred>
-    constexpr auto operator()(Pred pred) const -> folder_t<bool, impl_t<Pred>>
+    constexpr auto operator()(Pred pred) const -> reducer_proxy_t<bool, impl_t<Pred>>
     {
         return { Init, impl_t<Pred>{ std::move(pred) } };
     }
@@ -129,21 +129,117 @@ struct copy_fn
             return out;
         }
     };
+
     template <class Out>
-    constexpr auto operator()(Out out) const -> folder_t<Out, impl_t>
+    constexpr auto operator()(Out out) const -> reducer_proxy_t<Out, impl_t>
     {
         return { std::move(out), impl_t{} };
     }
 };
 
+template <class Impl>
+struct transducer_interface_t
+{
+    Impl m_impl;
+
+    template <class Reducer>
+    constexpr auto operator()(Reducer&& reducer) const -> std::invoke_result_t<Impl, Reducer>
+    {
+        return std::invoke(m_impl, std::forward<Reducer>(reducer));
+    }
+
+    template <class Reducer>
+    constexpr auto operator>>=(Reducer&& reducer) const -> std::invoke_result_t<Impl, Reducer>
+    {
+        return std::invoke(m_impl, std::forward<Reducer>(reducer));
+    }
+};
+
+struct transform_fn
+{
+    template <class Reducer, class Func>
+    struct reducer_t
+    {
+        Reducer m_next_reducer;
+        Func m_func;
+
+        template <class State, class... Args>
+        constexpr auto operator()(State state, Args&&... args) const -> step_t<State>
+        {
+            return m_next_reducer(std::move(state), std::invoke(m_func, std::forward<Args>(args)...));
+        }
+    };
+
+    template <class Func>
+    struct transducer_t
+    {
+        Func m_func;
+
+        template <class State, class Reducer>
+        constexpr auto operator()(reducer_proxy_t<State, Reducer> next) const
+            -> reducer_proxy_t<State, reducer_t<Reducer, Func>>
+        {
+            return { std::move(next.state), reducer_t<Reducer, Func>{ std::move(next.reducer), m_func } };
+        }
+    };
+
+    template <class Func>
+    constexpr auto operator()(Func&& func) const -> transducer_interface_t<transducer_t<std::decay_t<Func>>>
+    {
+        return { { std::forward<Func>(func) } };
+    }
+};
+
+struct filter_fn
+{
+    template <class Reducer, class Pred>
+    struct reducer_t
+    {
+        Reducer m_next_reducer;
+        Pred m_pred;
+
+        template <class State, class... Args>
+        constexpr auto operator()(State state, Args&&... args) const -> step_t<State>
+        {
+            if (std::invoke(m_pred, args...))
+            {
+                return m_next_reducer(std::move(state), std::forward<Args>(args)...);
+            }
+            return state;
+        }
+    };
+
+    template <class Pred>
+    struct transducer_t
+    {
+        Pred m_pred;
+
+        template <class State, class Reducer>
+        constexpr auto operator()(reducer_proxy_t<State, Reducer> next) const
+            -> reducer_proxy_t<State, reducer_t<Reducer, Pred>>
+        {
+            return { std::move(next.state), reducer_t<Reducer, Pred>{ std::move(next.reducer), m_pred } };
+        }
+    };
+
+    template <class Pred>
+    constexpr auto operator()(Pred&& pred) const -> transducer_interface_t<transducer_t<std::decay_t<Pred>>>
+    {
+        return { { std::forward<Pred>(pred) } };
+    }
+};
+
 }  // namespace detail
 
-constexpr inline auto fold = detail::fold_fn{};
+constexpr inline auto reduce = detail::reduce_fn{};
 
 constexpr inline auto all_of = detail::logical_sum_fn<std::logical_and<>, true, true>{};
 constexpr inline auto any_of = detail::logical_sum_fn<std::logical_or<>, false, true>{};
 constexpr inline auto none_of = detail::logical_sum_fn<std::logical_and<>, true, false>{};
 
 constexpr inline auto copy = detail::copy_fn{};
+
+constexpr inline auto transform = detail::transform_fn{};
+constexpr inline auto filter = detail::filter_fn{};
 
 }  // namespace cpp_reduce
--- tests/cpp-reduce.test.cpp
+++ tests/cpp-reduce.test.cpp
@@ -1,70 +1,129 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_all.hpp>
 #include <cpp-reduce/cpp-reduce.hpp>
+#include <sstream>
+
+constexpr inline struct str_fn
+{
+    template <class... Args>
+    std::string operator()(Args&&... args) const
+    {
+        std::stringstream ss;
+        (ss << ... << std::forward<Args>(args));
+        return ss.str();
+    }
+} str;
 
 TEST_CASE("simple reduce invocation", "")
 {
     const std::vector<int> input = { 2, 3, 5, 10 };
-    REQUIRE(cpp_reduce::fold(0, std::plus<>{})(input) == 20);
+    REQUIRE(cpp_reduce::reduce(0, std::plus<>{})(input) == 20);
 }
 
 TEST_CASE("all_of - regular implementation", "")
 {
-    const auto folder = cpp_reduce::fold(true, [](bool res, int value) -> bool {  //
+    const auto reducer = cpp_reduce::reduce(true, [](bool res, int value) -> bool {  //
         return res && (10 <= value && value <= 20);
     });
 
-    REQUIRE(folder(std::vector<int>{ 12, 13, 14, 19 }) == true);
-    REQUIRE(folder(std::vector<int>{}) == true);
-    REQUIRE(folder(std::vector<int>{ 12, 1401, 13, 14, 19 }) == false);
+    REQUIRE(reducer(std::vector<int>{ 12, 13, 14, 19 }) == true);
+    REQUIRE(reducer(std::vector<int>{}) == true);
+    REQUIRE(reducer(std::vector<int>{ 12, 1401, 13, 14, 19 }) == false);
 }
 
 TEST_CASE("all_of - short circuit implementation", "")
 {
-    const auto folder = cpp_reduce::fold(
+    const auto reducer = cpp_reduce::reduce(
         true,
         [](bool res, int value) -> cpp_reduce::step_t<bool>
         {  //
             return cpp_reduce::break_with_if(res && (10 <= value && value <= 20), [](bool v) { return !v; });
         });
 
-    REQUIRE(folder(std::vector<int>{ 12, 13, 14, 19 }) == true);
-    REQUIRE(folder(std::vector<int>{}) == true);
-    REQUIRE(folder(std::vector<int>{ 12, 1401, 13, 14, 19 }) == false);
+    REQUIRE(reducer(std::vector<int>{ 12, 13, 14, 19 }) == true);
+    REQUIRE(reducer(std::vector<int>{}) == true);
+    REQUIRE(reducer(std::vector<int>{ 12, 1401, 13, 14, 19 }) == false);
 }
 
 TEST_CASE("all_of - built-in implementation", "")
 {
-    const auto folder = cpp_reduce::all_of([](int value) { return 10 <= value && value <= 20; });
+    const auto reducer = cpp_reduce::all_of([](int value) { return 10 <= value && value <= 20; });
 
-    REQUIRE(folder(std::vector<int>{ 12, 13, 14, 19 }) == true);
-    REQUIRE(folder(std::vector<int>{}) == true);
-    REQUIRE(folder(std::vector<int>{ 12, 1401, 13, 14, 19 }) == false);
+    REQUIRE(reducer(std::vector<int>{ 12, 13, 14, 19 }) == true);
+    REQUIRE(reducer(std::vector<int>{}) == true);
+    REQUIRE(reducer(std::vector<int>{ 12, 1401, 13, 14, 19 }) == false);
 }
 
 TEST_CASE("any_of - built-in implementation", "")
 {
-    const auto folder = cpp_reduce::any_of([](int value) { return 10 <= value && value <= 20; });
+    const auto reducer = cpp_reduce::any_of([](int value) { return 10 <= value && value <= 20; });
 
-    REQUIRE(folder(std::vector<int>{ 12, 13, 14, 19 }) == true);
-    REQUIRE(folder(std::vector<int>{}) == false);
-    REQUIRE(folder(std::vector<int>{ 12, 1401, 13, 14, 19 }) == true);
+    REQUIRE(reducer(std::vector<int>{ 12, 13, 14, 19 }) == true);
+    REQUIRE(reducer(std::vector<int>{}) == false);
+    REQUIRE(reducer(std::vector<int>{ 12, 1401, 13, 14, 19 }) == true);
 }
 
 TEST_CASE("none_of - built-in implementation", "")
 {
-    const auto folder = cpp_reduce::none_of([](int value) { return 10 <= value && value <= 20; });
+    const auto reducer = cpp_reduce::none_of([](int value) { return 10 <= value && value <= 20; });
 
-    REQUIRE(folder(std::vector<int>{ 12, 13, 14, 19 }) == false);
-    REQUIRE(folder(std::vector<int>{}) == true);
-    REQUIRE(folder(std::vector<int>{ 12, 1401, 13, 14, 19 }) == false);
+    REQUIRE(reducer(std::vector<int>{ 12, 13, 14, 19 }) == false);
+    REQUIRE(reducer(std::vector<int>{}) == true);
+    REQUIRE(reducer(std::vector<int>{ 12, 1401, 13, 14, 19 }) == false);
 }
 
 TEST_CASE("copy", "")
 {
     std::vector<int> res = {};
-    const auto folder = cpp_reduce::copy(std::back_inserter(res));
-    folder(std::vector<int>{ 12, 13, 14, 19 });
-    folder(std::vector<int>{ 999, 990 });
+    const auto reducer = cpp_reduce::copy(std::back_inserter(res));
+    reducer(std::vector<int>{ 12, 13, 14, 19 });
+    reducer(std::vector<int>{ 999, 990 });
     REQUIRE_THAT(res, Catch::Matchers::RangeEquals({ 12, 13, 14, 19, 999, 990 }));
+}
+
+TEST_CASE("transform-copy", "")
+{
+    std::vector<int> res = {};
+    const auto reducer = cpp_reduce::transform([](int x) { return x * 10 + 1; })
+        >>= cpp_reduce::copy(std::back_inserter(res));
+    reducer(std::vector<int>{ 12, 13, 14, 19 });
+    REQUIRE_THAT(res, Catch::Matchers::RangeEquals({ 121, 131, 141, 191 }));
+}
+
+TEST_CASE("filter-copy", "")
+{
+    std::vector<int> res = {};
+    const auto reducer = cpp_reduce::filter([](int x) { return x < 14; }) >>= cpp_reduce::copy(std::back_inserter(res));
+    reducer(std::vector<int>{ 12, 13, 14, 19 });
+    REQUIRE_THAT(res, Catch::Matchers::RangeEquals({ 12, 13 }));
+}
+
+TEST_CASE("transform-filter-copy", "")
+{
+    std::vector<std::string> res = {};
+    const auto reducer = cpp_reduce::transform(str)  //
+        >>= cpp_reduce::filter([](const std::string& s) { return s.size() == 2; })
+        >>= cpp_reduce::copy(std::back_inserter(res));
+    reducer(std::vector<int>{ 1, 9, 12, 99, 101, 110 });
+    REQUIRE_THAT(res, Catch::Matchers::RangeEquals({ "12", "99" }));
+}
+
+TEST_CASE("transform-filter-all_of", "")
+{
+    std::vector<std::string> res = {};
+    const auto reducer = cpp_reduce::transform(str)  //
+        >>= cpp_reduce::filter([](const std::string& s) { return s.size() == 2; })
+        >>= cpp_reduce::all_of([](const std::string& s) { return s.size() == 2; });
+
+    REQUIRE(reducer(std::vector<int>{ 1, 9, 12, 99, 101, 110 }) == true);
+}
+
+TEST_CASE("filter-transform-copy", "")
+{
+    std::vector<std::string> res = {};
+    const auto reducer = cpp_reduce::filter([](int x) { return x % 2 == 0; })  //
+        >>= cpp_reduce::transform(str)                                         //
+        >>= cpp_reduce::copy(std::back_inserter(res));
+    reducer(std::vector<int>{ 1, 9, 12, 99, 101, 110 });
+    REQUIRE_THAT(res, Catch::Matchers::RangeEquals({ "12", "110" }));
 }
